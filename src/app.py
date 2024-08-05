5fd5cc7b488678f6344bef97eb72be1d7a10922b
Vibha Hegde <vibhavinayak99@gmail.com>
2024-08-05 12:16:57 -0700

    Dockerfile changes

--- src/app.py
+++ src/app.py
@@ -97,9 +97,3 @@ def chat_icon_conversation(resume_id):
         return jsonify({"error": str(e)}), 500
 
 
-
-if __name__ == '__main__':
-    # app.run(debug=True)
-    port = int(os.environ.get('PORT', 8080))
-    app.run(host='0.0.0.0', port=port)
-
